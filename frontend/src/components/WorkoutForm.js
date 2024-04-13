211b59904cde4751562c48ecea282b4549f0a07d
Prakhar <Prakhar.agra10@gmail.com>
2024-04-13 17:16:27 +0530

    added display and integrated frontedn and backend

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const WorkoutForm = () => {
+    const [title, setTitle] = useState('');
+    const [load, setLoad] = useState('');
+    const [reps, setReps] = useState('');
+    return (
+        <form className="create">
+    );
+}
+ 
+export default WorkoutForm;
